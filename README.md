54ba33b8e3386e194f2ca6322567cf1cc2c72837
Guilherme Haschel <ghaschel@gmail.com>
2022-08-05 17:14:15 -0300

    docs(updated documentation): updated docs

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# vscode-angular-html
+# vscode-angular-inline
 
-[![SemVer](https://img.shields.io/github/tag/ghaschel/vscode-angular-html.svg)](https://img.shields.io/github/tag/ghaschel/vscode-angular-html.svg)
-[![GitHub issues](https://img.shields.io/github/issues/ghaschel/vscode-angular-html.svg)](https://github.com/ghaschel/vscode-angular-html/issues)
-[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/ghaschel/vscode-angular-html/blob/master/LICENSE)
-[![Build status](https://travis-ci.org/ghaschel/vscode-angular-html.svg?branch=master)](https://travis-ci.org/ghaschel/vscode-angular-html.svg?branch=master)
-[![Installs](https://vsmarketplacebadge.apphb.com/installs-short/ghaschel.vscode-angular-html.svg?style=flat&color=blue)](https://marketplace.visualstudio.com/items?itemName=ghaschel.vscode-angular-html)
-[![Version](https://vsmarketplacebadge.apphb.com/version-short/ghaschel.vscode-angular-html.svg?style=flat&color=blue)](https://marketplace.visualstudio.com/items?itemName=ghaschel.vscode-angular-html) [![Greenkeeper badge](https://badges.greenkeeper.io/ghaschel/vscode-angular-html.svg)](https://greenkeeper.io/)
+[![SemVer](https://img.shields.io/github/tag/ghaschel/vscode-angular-inline.svg)](https://img.shields.io/github/tag/ghaschel/vscode-angular-inline.svg)
+[![GitHub issues](https://img.shields.io/github/issues/ghaschel/vscode-angular-inline.svg)](https://github.com/ghaschel/vscode-angular-inline/issues)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/ghaschel/vscode-angular-inline/blob/master/LICENSE)
+[![Build status](https://travis-ci.org/ghaschel/vscode-angular-inline.svg?branch=master)](https://travis-ci.org/ghaschel/vscode-angular-inline.svg?branch=master)
+[![Installs](https://vsmarketplacebadge.apphb.com/installs-short/ghaschel.vscode-angular-inline.svg?style=flat&color=blue)](https://marketplace.visualstudio.com/items?itemName=ghaschel.vscode-angular-inline)
+[![Version](https://vsmarketplacebadge.apphb.com/version-short/ghaschel.vscode-angular-inline.svg?style=flat&color=blue)](https://marketplace.visualstudio.com/items?itemName=ghaschel.vscode-angular-inline) [![Greenkeeper badge](https://badges.greenkeeper.io/ghaschel/vscode-angular-inline.svg)](https://greenkeeper.io/)
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 
 <div align="center">
-    <img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/angular-html.png" title="vscode-angular-html" alt="Logo" />
+    <img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/angular-inline.png" title="vscode-angular-inline" alt="Logo" />
 </div>
 
-## Angular HTML Template Syntax Highlighting
+## Angular Inline Template Syntax Highlighting
 
 This plugin adds syntax highlighting for angular HTML template files in VS Code. It supports the angular syntax itself, HTML DOM events and emphasizes deprecated and obsolete tags and attributes usage according to the current status of HTML.
 
@@ -21,87 +21,91 @@ This plugin adds syntax highlighting for angular HTML template files in VS Code.
 
 ## Summary
 
-- [Angular directives](#angular-directives)
-- [Doctype](#doctype)
-- [Comments](#comments)
-- [Invalid attributes](#invalid-attributes)
-- [Angular interpolations](#angular-interpolations)
-- [Angular structural tags](#angular-structural-tags)
-- [Entities](#html-entities)
-- [Regex](#regex)
-- [Deprecated tags](#deprecated-tags)
-- [DOM Events](#dom-events)
-- [Style inline](#style-inline)
-- [Style tag](#style-tag)
-- [Script tags](#script-tags)
-- [Generic attributes](#generic-attributes)
+- [vscode-angular-inline](#vscode-angular-inline)
+  - [Angular Inline Template Syntax Highlighting](#angular-inline-template-syntax-highlighting)
+  - [Summary](#summary)
+    - [Angular directives](#angular-directives)
+    - [Doctype](#doctype)
+    - [Comments](#comments)
+    - [Invalid attributes see complete list](#invalid-attributes-see-complete-list)
+    - [Angular interpolations (with safe navigation operator)](#angular-interpolations-with-safe-navigation-operator)
+    - [Angular structural tags](#angular-structural-tags)
+    - [Entities](#entities)
+    - [Regex](#regex)
+    - [Deprecated tags see complete list](#deprecated-tags-see-complete-list)
+    - [DOM events see complete list](#dom-events-see-complete-list)
+    - [Style inline](#style-inline)
+    - [Style tag (scss/stylus)](#style-tag-scssstylus)
+    - [Script tag](#script-tag)
+    - [Generic attributes](#generic-attributes)
+  - [Disclaimer](#disclaimer)
+  - [Changelog](#changelog)
 
 ### Angular directives
 
 <span name="angular-directives"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/angular-directives.gif" title="Angular directives" alt="Angular directives" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/angular-directives.gif" title="Angular directives" alt="Angular directives" />
 
 ### Doctype
 
 <span name="doctype"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/doctype.png" title="vscode-angular-html" alt="Doctype" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/doctype.png" title="vscode-angular-inline" alt="Doctype" />
 
 ### Comments
 
 <span name="comments"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/comments.png" title="vscode-angular-html" alt="Comments" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/comments.png" title="vscode-angular-inline" alt="Comments" />
 
 ### Invalid attributes [see complete list](DEPRECATED-ATTRIBUTES.md)
 
 <span name="invalid-attributes"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/invalid-attributes.png" title="Invalid attributes" alt="Invalid attributes" />
-
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/invalid-attributes.png" title="Invalid attributes" alt="Invalid attributes" />
 
 ### Angular interpolations (with safe navigation operator)
 
 <span name="angular-interpolations"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/angular-interpolations.png" title="Angular Interpolations" alt="Angular Interpolations" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/angular-interpolations.png" title="Angular Interpolations" alt="Angular Interpolations" />
 
 ### Angular structural tags
 
 <span name="angular-structural-tags"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/angular-structural-tags.png" title="Angular Structural Tags" alt="Angular Structural Tags" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/angular-structural-tags.png" title="Angular Structural Tags" alt="Angular Structural Tags" />
 
 ### Entities
 
 <span name="html-entities"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/html-entities.png" title="HTML Entities" alt="HTML Entities" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/html-entities.png" title="HTML Entities" alt="HTML Entities" />
 
 ### Regex
 
 <span name="regex"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/regex.png" title="vscode-angular-html" alt="Regex" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/regex.png" title="vscode-angular-inline" alt="Regex" />
 
 ### Deprecated tags [see complete list](DEPRECATED-TAGS.md)
 
 <span name="deprecated-tags"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/deprecated-tags.png" title="Deprecated tags" alt="Deprecated tags" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/deprecated-tags.png" title="Deprecated tags" alt="Deprecated tags" />
 
 ### DOM events [see complete list](DOM-EVENTS.md)
 
 <span name="dom-events"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/dom-events.png" title="DOM events" alt="DOM events" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/dom-events.png" title="DOM events" alt="DOM events" />
 
 ### Style inline
 
 <span name="style-inline"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/style-inline.png" title="Style inline" alt="Style inline" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/style-inline.png" title="Style inline" alt="Style inline" />
 
 ### Style tag (scss/stylus)
 
@@ -109,19 +113,19 @@ This plugin adds syntax highlighting for angular HTML template files in VS Code.
 
 > Note: stylus is only supported via [language-stylus](https://marketplace.visualstudio.com/items?itemName=sysoev.language-stylus)
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/style-tag.png" title="Style tag" alt="Style tag" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/style-tag.png" title="Style tag" alt="Style tag" />
 
 ### Script tag
 
 <span name="script-tags"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/script-tag.png" title="Script tags" alt="Script tag" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/script-tag.png" title="Script tags" alt="Script tag" />
 
 ### Generic attributes
 
 <span name="generic-attributes"></span>
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/generic-attributes.png" title="Generic attributes" alt="Generic attributes" />
+<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-inline/master/assets/generic-attributes.png" title="Generic attributes" alt="Generic attributes" />
 
 ## Disclaimer
 
---
+++ test/test.component.ts
@@ -0,0 +1,622 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: "vscode-angular-inline",
+  styleUrls: ["./vscode-angular-inline.scss"],
+  template: `
+
+    DOCTYPE
+
+    <!DOCTYPE html>
+    <!DOCTYPE HTML
+              PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+    COMMENTS
+
+    <!-- COMMENTS -->
+
+    INVALID ATTRIBUTES
+
+    <a charset="dasdasda"
+      coords=""
+      rev=""
+      shape=""></a>
+
+    <area nohref="" />
+
+    <body alink=""
+          background=""
+          bgcolor=""
+          link=""
+          vlink=""></body>
+
+    <br clear="" />
+
+    <col char=""
+        charoff=""
+        valign=""
+        width="" />
+
+    <colgroup align=""
+              char=""
+              charoff=""
+              valign=""
+              width=""></colgroup>
+
+    <dl compact=""></dl>
+
+    <menu compact=""></menu>
+
+    <head profile="test">
+      <title>Teste</title>
+    </head>
+
+    <hr align=""
+        noshade=""
+        size=""
+        width="" />
+
+    <html version="">
+
+    </html>
+
+    <iframe align=""
+            frameborder=""
+            longdesc=""
+            marginheight=""
+            marginwidth=""
+            scrolling=""></iframe>
+
+
+    <img align=""
+        hspace=""
+        longdesc=""
+        name=""
+        vspace="" />
+
+    <li type=""></li>
+
+    <link charset=""
+          rev=""
+          target="" />
+
+    <caption align=""></caption>
+
+    <div align=""></div>
+
+    <h1 align=""></h1>
+
+    <h2 align=""></h2>
+
+    <h3 align=""></h3>
+
+    <h4 align=""></h4>
+
+    <h5 align=""></h5>
+
+    <h6 align=""></h6>
+
+    <input align="" />
+
+    <legend align=""></legend>
+
+    <p align=""></p>
+
+    <ol compact=""
+        type=""></ol>
+
+    <ul compact="teste"
+        type=""></ul>
+
+    <object archive=""
+            border=""
+            classid=""
+            codebase=""
+            codetype=""
+            declare=""
+            hspace=""
+            standby=""
+            vspace=""
+            align=""></object>
+
+    <param type=""
+          valuetype="" />
+
+
+    <pre width=""></pre>
+
+    <t abbr=""
+      axis=""></t>
+
+    <table bgcolor=""
+          border=""
+          cellpadding=""
+          cellspacing=""
+          frame=""
+          rules=""
+          scheme=""
+          width=""></table>
+
+    <tbody align=""
+          char=""
+          charoff=""
+          valign=""></tbody>
+
+    <td abbr=""
+        align=""
+        axis=""
+        bgcolor=""
+        char=""
+        charoff=""
+        nowrap=""
+        scope=""
+        valign=""
+        width=""></td>
+
+    <tfoot align=""
+          char=""
+          charoff=""
+          valign=""></tfoot>
+
+    <th align=""
+        bgcolor=""
+        char=""
+        charoff=""
+        nowrap=""
+        valign=""
+        width=""></th>
+
+    <thead align=""
+          char=""
+          charoff=""
+          valign=""></thead>
+
+    <tr align=""
+        bgcolor=""
+        char=""
+        charoff=""
+        valign=""></tr>
+
+    <div-test></div-test>
+    <menu-test></menu-test>
+
+    ANGULAR DIRECTIVES
+
+    <div [binding]="to.element"
+        [binding]="to.function('with-parameter')"
+        @animationTrigger
+        [element_binding]="to.function_with_underline('with-parameter')"
+        (click)="to.function(with.parameter)"
+        (click)="to.function(with!.parameter)"
+        (click)="to.function(with?.parameter)"
+        [(binding)]="to.element"
+        [attr.href]="file?.link"
+        [style.font-size.px]="10"
+        [@$visi_bility]="module.$visibility"
+        [@visibility]="getMenuState()"
+        [@visibility.done]="doSomething()"
+        [(binding)]="to.function('with-parameter')"
+        (click)="callback(parameter.test)"
+        mat-header-cell
+        *ngFor="let obs of observable | async as test;"
+        *matHeaderCellDef
+        class="test"
+        [ngClass]="{
+            'test': test.isTruthy()
+        }"
+        *
+        ref-variable
+        ref-assingVariable="test"
+        let-col$="test"
+        let-col$
+        let-i="index"
+        *ngIf="condition.met()"
+        *ngIf="condition?.met"
+        [ngClass]="{ 'test': sdsdasda }"
+        class="testing with non multiline {{test}}"
+        test
+        [test]="test.test();"
+        *ngIf="selectedCustomer && selectedCustomer.id > 0; else block1; then block2"
+        *ngFor="let a of b['a'][teste].asd; index as c"
+        #templateVariable
+        test></div>
+
+    ANGULAR INTERPOLATIONS
+
+    {{ test | pipe }}
+    {{ test | pipe:'withParameters' }}
+    {{ angular.interpolations || test }}
+    {{ angular?.interpolations }}
+    {{ angular!.interpolations }}
+
+    <span>
+      Hello {{ 1 + 1 === 3 ? 'me' : 'you' }}
+    </span>
+    {{ user?.preferences?.avatar }}
+    {{ user?.name | uppercase }}
+    {{ user!.preferences!.nickName | uppercase }}
+
+    ANGULAR STRUCTURAL TAGS
+
+    <ng-content>
+      <span [class]="test()"></span>
+    </ng-content>
+
+    <ng-container *ngIf="condition?.met">
+      <test-component></test-component>
+    </ng-container>
+
+    <ng-template #noPublishedDate>
+      No published date
+    </ng-template>
+
+    <router-outlet></router-outlet>
+
+    <div *
+        test="">
+    </div>
+
+    <input type="text"
+          #color
+          placeholder="Hex color value">
+    <button (click)="printValue(color.value)">
+      Show color
+    </button>
+    <span>
+      Published: {{ publishedDate | date:'fullDate' }}
+    </span>
+
+    <span *ngIf="publishedDate">
+      Published: {{ publishedDate | date:'fullDate' }}
+    </span>
+
+    <span *ngIf="publishedDate; else noPublishedDate">
+      Published: {{ publishedDate | date:'fullDate' }}
+    </span>
+
+    <ul>
+      <li *ngFor="let user of users">
+        name: {{ user.name }}, email: {{ user.email }}
+      </li>
+    </ul>
+
+    <ul>
+      <li *ngFor="let user of users | async as test; index as i;">
+        {{ i + 1 }}- name: {{ user.name }}, email: {{ user.email }}
+      </li>
+    </ul>
+
+    <div [ngSwitch]="user.gender">
+      <span *ngSwitchCase="'m'">Male</span>
+      <span *ngSwitchCase="'f'">Female</span>
+      <span *ngSwitchDefault>Not provided</span>
+    </div>
+
+    ENTITIES
+
+    &nbsp;
+    &#0098;
+
+    REGEX
+
+    /[^@\/#(){31231231}]/
+
+    DEPRECATED TAGS
+
+    <acronym class=""></acronym>
+
+    <applet class=""></applet>
+
+    <basefont class="" />
+
+    <big class=""></big>
+
+    <center class=""></center>
+
+    <dir class=""></dir>
+
+    <font class=""></font>
+
+    <frame class="" />
+
+    <frameset class="">
+
+    </frameset>
+
+    <isindex class="dsadsdasd" />
+
+    <noframes class="">
+
+    </noframes>
+
+    <strike class="ste">teste testestestestes </strike>
+
+    <tt> </tt>
+
+    <u>
+      the single quote won't bother anymore
+      neither will the double one "
+    </u>
+
+
+    DOM EVENTS
+
+    <div DOMMenuItemActive="callback()"
+        DOMMenuItemInactive="callback()"
+        DOMMouseScroll="callback()"
+        onabort="callback()"
+        onafterprint="callback()"
+        onafterupdate="callback()"
+        onbeforecopy="callback()"
+        onbeforecut="callback()"
+        onbeforeeditfocus="callback()"
+        onbeforepaste="callback()"
+        onbeforeprint="callback()"
+        onbeforeunload="callback()"
+        onbeforeupdate="callback()"
+        onblur="callback()"
+        onbounce="callback()"
+        onbroadcast="callback()"
+        oncanplay="callback()"
+        oncanplaythrough="callback()"
+        oncellchange="callback()"
+        onchange="callback()"
+        onclick="callback()"
+        onclose="callback()"
+        oncommand="callback()"
+        oncommandupdate="callback()"
+        oncontextmenu="callback()"
+        oncopy="callback()"
+        oncut="callback()"
+        ondataavailable="callback()"
+        ondatasetchanged="callback()"
+        ondatasetcomplete="callback()"
+        ondblclick="callback()"
+        ondrag="callback()"
+        ondragdrop="callback()"
+        ondragend="callback()"
+        ondragenter="callback()"
+        ondragexit="callback()"
+        ondraggesture="callback()"
+        ondragleave="callback()"
+        ondragover="callback()"
+        ondragstart="callback()"
+        ondrop="callback()"
+        ondurationchange="callback()"
+        onemptied="callback()"
+        onended="callback()"
+        onerror="callback()"
+        onerrorupdate="callback()"
+        onfilterchange="callback()"
+        onfinish="callback()"
+        onfocus="callback()"
+        onformchange="callback()"
+        onforminput="callback()"
+        ongotpointercapture="callback()"
+        onhelp="callback()"
+        oninput="callback()"
+        oninvalid="callback()"
+        onkeydown="callback()"
+        onkeypress="callback()"
+        onkeyup="callback()"
+        onload="callback()"
+        onloadeddata="callback()"
+        onloadedmetadata="callback()"
+        onloadstart="callback()"
+        onlosecapture="callback()"
+        onlostpointercapture="callback()"
+        onmousedown="callback()"
+        onmousemove="callback()"
+        onmouseout="callback()"
+        onmouseover="callback()"
+        onmouseup="callback()"
+        onmousewheel="callback()"
+        onoverflow="callback()"
+        onoverflowchanged="callback()"
+        onpaste="callback()"
+        onpause="callback()"
+        onplay="callback()"
+        onplaying="callback()"
+        onpointercancel="callback()"
+        onpointerdown="callback()"
+        onpointerenter="callback()"
+        onpointerleave="callback()"
+        onpointermove="callback()"
+        onpointerout="callback()"
+        onpointerover="callback()"
+        onpointerup="callback()"
+        onpopuphidden="callback()"
+        onpopuphiding="callback()"
+        onpopupshowing="callback()"
+        onpopupshown="callback()"
+        onprogress="callback()"
+        onpropertychange="callback()"
+        onratechange="callback()"
+        onreadystatechange="callback()"
+        onreset="callback()"
+        onresize="callback()"
+        onrowenter="callback()"
+        onrowexit="callback()"
+        onrowinserted="callback()"
+        onrowsdelete="callback()"
+        onscroll="callback()"
+        onseeked="callback()"
+        onseeking="callback()"
+        onselect="callback()"
+        onselectstart="callback()"
+        onshow="callback()"
+        onstalled="callback()"
+        onstart="callback()"
+        onstop="callback()"
+        onsubmit="callback()"
+        onsuspend="callback()"
+        ontimeupdate="callback()"
+        onunderflow="callback()"
+        onunload="callback()"
+        onvolumechange="callback()"
+        onwaiting="callback()"></div>
+
+    STYLE INLINE
+
+    <div style="display: block;
+                background-color: rgb(255, 255, 255);
+                color: rgba(33, 254, 12, 1);"></div>
+
+    GENERIC ATTRIBUTES
+
+    <div class="test"
+        id="test"
+        aria-label="test"
+        no-value
+        novalidate></div>
+
+    <input type="text">
+
+    <label class="test"></label>
+
+    <button [attr.aria-label]="'test'"></button>
+
+    <button [style.background-color]="canSave ? 'cyan': 'grey'">Save</button>
+
+    <button [style.font-size.em]="isSpecial ? 3 : 1">Big</button>
+
+    <style>
+      // stylus
+      .stylus .way &-of-doing-it[with-attr] color: red;
+
+      // scss
+      .class {
+        .with {
+          &-children[with-attr] {
+            &:hover {
+              &::before {
+                display: inline-block;
+              }
+            }
+          }
+        }
+      }
+
+    </style>
+
+    <script type="text/javascript">
+      let a = '123';
+      const b = 123;
+      new Class();
+
+      function test() {
+        test.callback();
+      }
+
+    </script>
+
+    <ng-container></ng-container>
+    <ng-content></ng-content>
+    <ng-template></ng-template>
+    <router-outlet></router-outlet>
+    <mat-accordion></mat-accordion>
+    <mat-action-list></mat-action-list>
+    <mat-action-row></mat-action-row>
+    <mat-autocomplete></mat-autocomplete>
+    <mat-card-actions></mat-card-actions>
+    <mat-card-content></mat-card-content>
+    <mat-card-header></mat-card-header>
+    <mat-card-footer></mat-card-footer>
+    <mat-card-subtitle></mat-card-subtitle>
+    <mat-card-title></mat-card-title>
+    <mat-card-title-group></mat-card-title-group>
+    <mat-card></mat-card>
+    <mat-checkbox></mat-checkbox>
+    <mat-chip-list></mat-chip-list>
+    <mat-datepicker-toggle></mat-datepicker-toggle>
+    <mat-datepicker></mat-datepicker>
+    <mat-drawer-content></mat-drawer-content>
+    <mat-drawer></mat-drawer>
+    <mat-divider></mat-divider>
+    <mat-error></mat-error>
+    <mat-expansion-panel-header></mat-expansion-panel-header>
+    <mat-expansion-panel></mat-expansion-panel>
+    <mat-form-field></mat-form-field>
+    <mat-grid-list></mat-grid-list>
+    <mat-grid-tile-footer></mat-grid-tile-footer>
+    <mat-grid-tile-header></mat-grid-tile-header>
+    <mat-grid-title></mat-grid-title>
+    <mat-hint></mat-hint>
+    <mat-icon></mat-icon>
+    <mat-label></mat-label>
+    <mat-list-item></mat-list-item>
+    <mat-list-option></mat-list-option>
+    <mat-list></mat-list>
+    <mat-menu></mat-menu>
+    <mat-nav-list></mat-nav-list>
+    <mat-optgroup></mat-optgroup>
+    <mat-option></mat-option>
+    <mat-panel-description></mat-panel-description>
+    <mat-panel-title></mat-panel-title>
+    <mat-radio-button></mat-radio-button>
+    <mat-radio-group></mat-radio-group>
+    <mat-selection-list></mat-selection-list>
+    <mat-select-trigger></mat-select-trigger>
+    <mat-select></mat-select>
+    <mat-sidenav-content></mat-sidenav-content>
+    <mat-sidenav-container></mat-sidenav-container>
+    <mat-sidenav></mat-sidenav>
+    <mat-slide-toggle></mat-slide-toggle>
+    <mat-slider></mat-slider>
+    <mat-toolbar-row></mat-toolbar-row>
+    <mat-toolbar></mat-toolbar>
+    <mat-horizontal-stepper></mat-horizontal-stepper>
+    <mat-pseudo-checkbox></mat-pseudo-checkbox>
+    <mat-step></mat-step>
+    <mat-vertical-stepper></mat-vertical-stepper>
+    <mat-tab-group></mat-tab-group>
+    <mat-tab></mat-tab>
+    <mat-nav-tab-bar></mat-nav-tab-bar>
+    <mat-tree-node></mat-tree-node>
+    <mat-tree></mat-tree>
+    <mat-nested-tree-node></mat-nested-tree-node>
+    <mat-button-toggle-group></mat-button-toggle-group>
+    <mat-button-toggle></mat-button-toggle>
+    <mat-chip-list></mat-chip-list>
+    <mat-chip></mat-chip>
+    <mat-basic-chip></mat-basic-chip>
+    <mat-progress-spinner></mat-progress-spinner>
+    <mat-spinner></mat-spinner>
+    <mat-progress-bar></mat-progress-bar>
+    <mat-dialog-content></mat-dialog-content>
+    <mat-dialog-actions></mat-dialog-actions>
+    <mat-paginator></mat-paginator>
+    <mat-text-column></mat-text-column>
+    <mat-header-row></mat-header-row>
+    <mat-header-cell></mat-header-cell>
+    <mat-table></mat-table>
+    <mat-cell></mat-cell>
+    <mat-row></mat-row>
+    <cdk-step></cdk-step>
+    <cdk-virtual-scroll-viewport></cdk-virtual-scroll-viewport>
+    <cdk-header-cell></cdk-header-cell>
+    <cdk-cell></cdk-cell>
+    <cdk-table></cdk-table>
+    <cdk-header-row></cdk-header-row>
+    <cdk-row></cdk-row>
+    <cdk-tree-node></cdk-tree-node>
+    <cdk-tree></cdk-tree>
+    <cdk-nested-tree-node></cdk-nested-tree-node>
+    <mat-chip-list></mat-chip-list>
+
+    <mat-chip-avatar let-item$="item$"></mat-chip-avatar>
+    <mat-date-range-input></mat-date-range-input>
+    <mat-date-range-picker></mat-date-range-picker>
+    <mat-datepicker-actions></mat-datepicker-actions>
+    <mat-calendar></mat-calendar>
+    <mat-grid-tile></mat-grid-tile>
+    <mat-drawer-container></mat-drawer-container>
+    <mat-stepper></mat-stepper>
+    <mat-tab-nav-panel></mat-tab-nav-panel>
+    <cdk-accordion></cdk-accordion>
+    <cdk-accordion-item></cdk-accordion-item>
+    <cdk-footer-cell></cdk-footer-cell>
+    <cdk-footer-row></cdk-footer-row>
+  `,
+})
+export class VscodeAngularInline {}
